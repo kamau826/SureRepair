8ed7fe951b3f378b7077f585a2a5e84c9ca6766b
kamau <kamau4542@gmail.com>
2022-07-26 17:12:14 +0300

    added function for saving device images

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,11 @@ from flask_login import current_user,login_user,logout_user,login_required
 from datetime import date
 import string
 import secrets
+import os
+
+
+UPLOAD_FOLDER = 'app/static/img'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 loginmanager.login_view='login'
 
@@ -55,22 +60,37 @@ def index():
     return render_template('index.html',devices=devices)
 
 
+
+#function to save device images
+def save_device_img(device_image):
+    random_hex = secrets.token_hex(8)
+    _, f_text = os.path.splitext(device_image.filename)
+    picture_fn = random_hex + f_text
+    picture_path = os.path.join(app.root_path, 'static/deviceimg', picture_fn)
+    device_image.save(picture_path)
+    return picture_fn
+
+
 #book repair route
 @app.route('/book_repair',methods=['GET','POST'])
 @login_required
 def book_repair():
     if request.method == 'POST':
-        alphabet = string.ascii_letters + string.digits
-        device_key = ''.join(secrets.choice(alphabet) for i in range(32))
-        name=request.form['device_name']
-        model=request.form['model']
-        fault=request.form['fault']
-        user_id=current_user.id
-        device=Device(name=name,model=model,fault=fault,user_id=user_id,device_key=device_key)
-        db.session.add(device)
-        db.session.commit()
-        flash('device booked successfully')
-        return redirect(url_for('index'))
+        if request.files:
+            alphabet = string.ascii_letters + string.digits
+            device_key = ''.join(secrets.choice(alphabet) for i in range(32))
+            name=request.form['device_name']
+            model=request.form['model']
+            fault=request.form['fault']
+            image = request.files['device_image']
+            device_image = save_device_img(image)
+            user_id=current_user.id
+            device=Device(name=name,model=model,fault=fault,user_id=user_id,device_key=device_key, device_image=device_image)
+            db.session.add(device)
+            db.session.commit()
+
+            flash('device booked successfully')
+            redirect(url_for('index'))
 
     return render_template('booking_repair.html')
 
@@ -79,7 +99,8 @@ def book_repair():
 @login_required
 def view_device(dvk):
     device=Device.query.filter_by(device_key=dvk).first()
-    return render_template('view_device.html',device=device)
+    device_image = url_for('static', filename='deviceimg/' + device.device_image)
+    return render_template('view_device.html',device=device, device_image=device_image)
 
 @app.route('/technician/<usk>',methods=['GET','POST'])
 @login_required
@@ -104,8 +125,6 @@ def end_repair(dvk):
     device=Device.query.filter_by(device_key=dvk).first()
     
     if request.method=='POST':
-
-        
         device.tech_resolution=request.form['tech_resolution']
         device.repair_price= request.form['repair_price']
         
@@ -113,9 +132,6 @@ def end_repair(dvk):
         db.session.add(device)
         db.session.commit()
     return redirect(url_for('view_device',dvk=dvk))
-
-
-
 
 
 
