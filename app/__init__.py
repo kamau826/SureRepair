19578bf0c9c7793df6c1052aed0cc49a8e583194
kamau <kamau4542@gmail.com>
2022-08-17 13:11:41 +0300

    initialized flask excel

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import flask_excel as excel
 
 
 app = Flask(__name__)
@@ -10,5 +11,6 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 loginmanager=LoginManager(app)
+excel.init_excel(app)
 
 from app import routes, models
