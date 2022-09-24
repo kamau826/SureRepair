05027d93ecbfa7fcc3492e2c232d67b341aac658
kiptoo <denysmutay@gmail.com>
2022-09-24 12:25:09 +0300

    database configurations

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'justloveJesus'
-    SQLALCHEMY_DATABASE_URI = 'postgres://tjckjhlcczudoz:c1f8b346c9c6089944b84c008dbde3d9de09ce92e090e97032e298af8f713f96@ec2-3-214-2-141.compute-1.amazonaws.com:5432/dbs6pkd91t2if7'
+    SQLALCHEMY_DATABASE_URI = 'postgres://vbrnxqyfjgsbgt:d617f660feb8cabba76189f942cc16b26c1a43814c13df1e9f3885c6d091c5ac@ec2-54-85-56-210.compute-1.amazonaws.com:5432/defm02tgd6cba6'
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
      #                         'sqlite:///' + os.path.join(basedir, 'repairShop.db')
